9dd0d6f6b2de73f1e41025bb26d1fb15d2000e89
Reisen <reisen@morphism.org>
2015-06-28 17:19:19 +0100

    Fix importing main module.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tornado.ioloop import IOLoop
 from tornado.web import Application, url
-from views import *
 import os
 
 
@@ -13,21 +12,52 @@ settings = {
     'cookie_secret': b'\r\xc7\xa0\x1d:\xa32\x12\x82q\x89\x8d\xe9ZP1G\xe2f\xa0\x83\x0bB\xa99\x11e\x1a^\xd9\x16)',
     'xsrf_cookies': True,
     'static_path': os.path.join(os.path.dirname(__file__), 'assets'),
-    'autoreload': True,
-    'url_map': [
-        url(r'/admin/?',   admin.Index),
-        url(r'/snippet/?', front.Snippet),
-        url(r'/login/?',   front.Login),
-        url(r'/captcha/?', front.Captcha),
-        url(r'/(.+?)?/?',  front.Serve),
-    ]
+    'autoreload': True
 }
 
 
 #-------------------------------------------------------------------------------
 
 
+import smtplib
+
+class Mail:
+    def __init__(self):
+        self.server = smtplib.SMTP(settings['smtp_host'], settings['smtp_port'])
+        self.server.ehlo()
+        self.server.starttls()
+        self.server.login(settings['user'], settings['pass'])
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.server.close()
+
+    def mail(self, message, **kwargs):
+        headers = '\n'.join(': '.join(header) for header in kwargs.items())
+        message = "{}\n\n{}".format(headers, message)
+        self.server.sendmail(
+            kwargs.get('From', settings['smtp_from']),
+            kwargs.get('To', '').split(', '),
+            message
+        )
+
+
+#-------------------------------------------------------------------------------
+
+
 if __name__ == '__main__':
+    from views import *
+
+    settings['url_map'] = [
+        url(r'/admin/?',   admin.Index),
+        url(r'/snippet/?', front.Snippet),
+        url(r'/login/?',   front.Login),
+        url(r'/captcha/?', front.Captcha),
+        url(r'/(.+?)?/?',  front.Serve),
+    ]
+
     with open('schema.sql') as f:
         import sqlite3
         db = sqlite3.connect('data.db')
--- views/__init__.py
+++ views/__init__.py
@@ -6,37 +6,6 @@ import time
 import datetime
 
 
-#-------------------------------------------------------------------------------
-
-
-import smtplib
-
-class Mail:
-    def __init__(self):
-        self.server = smtplib.SMTP(settings['smtp_host'], settings['smtp_port'])
-        self.server.ehlo()
-        self.server.starttls()
-        self.server.login(settings['user'], settings['pass'])
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.server.close()
-
-    def mail(self, message, **kwargs):
-        headers = '\n'.join(': '.join(header) for header in kwargs.items())
-        message = "{}\n\n{}".format(headers, message)
-        self.server.sendmail(
-            kwargs.get('From', settings['smtp_from']),
-            kwargs.get('To', '').split(', '),
-            message
-        )
-
-
-#-------------------------------------------------------------------------------
-
-
 html = re.compile(r'<(\w+).*>', re.M)
 
 def detecthtml(s):
